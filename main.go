5cfcba585c2b095591be88471a771b302cf0f0ef
Joseph Spurrier <github@josephspurrier.com>
2022-02-24 13:23:02 -0500

    Rename kion folder

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-	"github.com/kionsoftware/terraform-provider-kion/cloudtamerio"
 )
 
 // Generate docs for website
