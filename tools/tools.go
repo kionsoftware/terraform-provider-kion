a4213beade2fbd5b437bb748f41612b041b50dc8
namin <namin@kion.io>
2023-03-09 17:54:45 -0500

    revert doc dependency

--- tools/tools.go
+++
@@ -1,9 +0,0 @@
-//go:build tools
-// +build tools
-
-package tools
-
-import (
-	// Ensure documentation generator is not removed from go.mod.
-	_ "github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs"
-)
