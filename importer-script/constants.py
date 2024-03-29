ddc98f1bf7fbfa3728156e87178924f8a89b7391
bshutterkion <bshutter@kion.io>
2024-03-29 16:57:48 -0600

    refactor importer script

---
+++ importer-script/constants.py
@@ -0,0 +1,12 @@
+from config import ARGS
+
+RESOURCE_PREFIX = 'kion'
+BASE_URL = "%s/api" % ARGS.kion_url
+HEADERS = {"accept": "application/json",
+           "Authorization": "Bearer " + ARGS.kion_api_key}
+
+MAX_UNAUTH_RETRIES = 15
+UNAUTH_RETRY_COUNTER = 0
+
+IMPORTED_MODULES = []
+IMPORTED_RESOURCES = []
