8cbd9bbfe0caf910a74da631087fe595bbc265cd
bshutterkion <bshutter@kion.io>
2025-10-01 13:32:58 -0600

    v0.3.29 Release
    [Add support custom accounts](https://support.kion.io/hc/en-us/articles/39072743302157-Custom-Accounts-Custom-Billing-Sources)

---
+++ examples/resources/kion_custom_account/resource.tf
@@ -0,0 +1,18 @@
+# Create a custom account in a project with all available options
+resource "kion_custom_account" "complete_example" {
+  # Required fields
+  name           = "Terraform Complete Custom Account"
+  payer_id       = 1
+  account_number = "CUSTOM-123456"
+
+  # Optional fields
+  project_id          = 42
+  start_datecode      = "2024-03"
+
+  # Labels for the account (only supported for project accounts)
+  labels = {
+    environment = "production"
+    team        = "platform"
+    cost_center = "12345"
+  }
+}
